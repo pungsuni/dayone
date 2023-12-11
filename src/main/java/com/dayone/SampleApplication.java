04f638700fb6e0edbc623dfe61591217dc6c763e
pungsuni <dlwldud000223@gmail.com>
2023-12-11 21:50:20 +0900

    first commit

---
+++ src/main/java/com/dayone/SampleApplication.java
@@ -0,0 +1,20 @@
+package com.dayone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TreeMap;
+
+
+@SpringBootApplication
+@EnableScheduling
+@EnableCaching
+public class SampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+
+    }
+}
